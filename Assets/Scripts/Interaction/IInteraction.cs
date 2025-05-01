d323c5f04b231f9007fecf32ab4b7b6a736d8b36
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2025-05-01 14:34:28 +0900

    feat :  플레이어 상호작용 구현
    자식 오브젝트에 상호작용 영역을 만들어서 하기로 결정

--- Assets/Scripts/Interaction/IInteraction.cs
+++ Assets/Scripts/Interaction/IInteraction.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface IInteraction 
-{
+{   
+    //상호작용 인터페이스
+    //상호작용 가능한 오브젝트의 스크립트 컴포넌트에게 상속
     void Interact();
 }
---
+++ Assets/Scripts/Player/PlayerInteration.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class PlayerInteration : MonoBehaviour
+{
+    //플레이어의 상호작용 로직 구현
+    //f키를 통해 상호작용
+
+    private Collider2D currentTarget;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out IInteraction interactable))
+        {
+            currentTarget = other;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (currentTarget == other)
+        {
+            currentTarget = null;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && currentTarget != null)
+        {
+            currentTarget.GetComponent<IInteraction>()?.Interact();
+        }
+    }
+}
